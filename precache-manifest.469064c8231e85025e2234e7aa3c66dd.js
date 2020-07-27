4a00b31ce8c547f47e33bd8a1f047c6fe6a28459
Colin Groark <cgroark@enviroissues.com>
2020-07-27 16:59:49 -0700

    test build

---
+++ precache-manifest.469064c8231e85025e2234e7aa3c66dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b13db150e270fee26ebc9be09ab00d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2104ee6805234caeda3a",
+    "url": "/static/css/2.fcccc91c.chunk.css"
+  },
+  {
+    "revision": "fad38b0dd3d054fd408f",
+    "url": "/static/css/main.f4529beb.chunk.css"
+  },
+  {
+    "revision": "2104ee6805234caeda3a",
+    "url": "/static/js/2.35cfcdf8.chunk.js"
+  },
+  {
+    "revision": "0e479e34c7853cf26f3584614b8f1e58",
+    "url": "/static/js/2.35cfcdf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fad38b0dd3d054fd408f",
+    "url": "/static/js/main.1b27ec4e.chunk.js"
+  },
+  {
+    "revision": "c06a26c6f60a9a6a6e16",
+    "url": "/static/js/runtime-main.3234d57e.js"
+  },
+  {
+    "revision": "f28cc09a04e2fa76b2e8778dcd2cabe7",
+    "url": "/static/media/book-stack.f28cc09a.jpeg"
+  },
+  {
+    "revision": "c6e5ed7e0d64885ddfb48e57c6e2c74f",
+    "url": "/static/media/mug-book.c6e5ed7e.png"
+  },
+  {
+    "revision": "c95a880f9282d19014df9d7f0cb0bc0c",
+    "url": "/static/media/open-book.c95a880f.png"
+  }
+]);
